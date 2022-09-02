b30803ed1d7e560fa827f284c53f86c344c56713
Prakash <prakashkalsariya@gmail.com>
2022-09-02 19:00:20 +0530

    app import change

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import "./App.scss";
 import Slider from "./component/slider/Slider";
-import RotateArrow from "./component/rotateArrow/RotateArrow";
 import Home from "./pages/home/Home";
 
 
 function App() {
   return (
     <div className="main-container">
-      {/* <Slider /> */}
-      {/* <RotateArrow/> */}
+      <Slider /> 
       <Home/>
     </div>
   );
