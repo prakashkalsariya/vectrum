c330b011c828de079a548885076289b3be6dc4cb
Prakash <prakashkalsariya@gmail.com>
2022-09-02 12:31:33 +0530

    import changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import "./App.scss";
 import Slider from "./component/slider/Slider";
+import RotateArrow from "./component/rotateArrow/RotateArrow";
+
 
 function App() {
   return (
-    <div className="body">
-      <Slider />
+    <div className="main-container">
+      {/* <Slider /> */}
+      <RotateArrow/>
     </div>
   );
 }
